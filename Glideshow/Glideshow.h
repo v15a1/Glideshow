fc12d9a3e6f6362a12d3f863fb42a19945bc0c44
Visal Rajapakse <visalrajapakse@gmail.com>
2021-02-28 10:14:05 +0530

    project setup

---
+++ Glideshow/Glideshow.h
@@ -0,0 +1,18 @@
+//
+//  Glideshow.h
+//  Glideshow
+//
+//  Created by Visal Rajapakse on 2021-02-28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Glideshow.
+FOUNDATION_EXPORT double GlideshowVersionNumber;
+
+//! Project version string for Glideshow.
+FOUNDATION_EXPORT const unsigned char GlideshowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Glideshow/PublicHeader.h>
+
+
---
+++ GlideshowExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GlideshowExample
+//
+//  Created by Visal Rajapakse on 2021-02-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
